752395ac40839e35a16b8922426c5f33986f6099
Lana Tomlin <lana.motom@gmail.com>
2022-07-23 14:45:16 -0400

    path following

--- SYDE351Project.m
+++ SYDE351Project.m
@@ -45,36 +45,43 @@ sim("Suspension_System.slx", 5);
 %% Path Planning
 t = 0;
 % Drive left for 15m, driving backwards
-[v_l, v_r, t_new] = path_linear(13.5, false, 0, 0);
+[v_l, v_r, t_new] = path_linear(15-1.2, false, 0, 0);
 v_left = v_l;
 v_right = v_r;
 t = t + t_new;
 
 % Rotate 90 deg CCW
-[v_l, v_r, t_new] = path_rot(90.3999, false, 24*(0.46), 24*(0.46));
+[v_l, v_r, t_new] = path_rot(90, true, 24*(0.46), 24*(0.46));
 % Accelerate
 v_left = [v_left, v_l];
 v_right = [v_right, v_r];
 t = t + t_new;
  
- % Move 10m Foreward
-[v_l, v_r, t_new] = path_linear(10, true, 0, 0);
+%  % Move 10m Foreward
+[v_l, v_r, t_new] = path_linear(12.2, true, 24*(0.46), 24*(0.46));
 v_left = [v_left, v_l];
 v_right = [v_right, v_r];
 t = t + t_new;
-% 
+
 % % Rotate 90 deg CW
-[v_l, v_r, t_new] = path_rot(90, true, 0, 0);
+[v_l, v_r, t_new] = path_rot(90, false, 24*(0.46), 24*(0.46));
 % Accelerate
 v_left = [v_left, v_l];
 v_right = [v_right, v_r];
 t = t + t_new;
-% 
+
 % % Move 8m Foreward
-% [v_l, v_r, t_new] = path_linear(8, true);
-% v_left = [v_left, v_l];
-% v_right = [v_right, v_r];
-% t = t + t_new;
+[v_l, v_r, t_new] = path_linear(8+1.5, true, 24*(0.46), 24*(0.46));
+v_left = [v_left, v_l];
+v_right = [v_right, v_r];
+t = t + t_new;
+
+% %Stop
+[v_l, v_r, t_new] = path_stop(24*(0.46), 5)
+v_left = [v_left, v_l];
+v_right = [v_right, v_r];
+ t = t + t_new;
+% 
 % 
 % % Generate total time and timeseries:
 timescale = linspace(0, t, length(v_right));
@@ -87,13 +94,13 @@ timescale = linspace(0, t, length(v_right));
 left_motor_voltage = timeseries(v_left, timescale);
 right_motor_voltage = timeseries(v_right, timescale);
 
-path = sim("WheelDriveModel.slx", 60);
+path = sim("WheelDriveModel.slx", 100);
 
 function [v_path_left, v_path_right, turn_time] = path_rot(target_angle, CW, right_voltage_start, left_voltage_start)
     accel_time = 5;
-    turning_vel = 11.3; % deg/s
+    turning_vel = 11.25000001; % deg/s
     accel_rot = accel_time*turning_vel/2;
-    deccel_time = 10;
+    deccel_time = 5;
     deccel_rot = accel_time*turning_vel/2;
     turning_voltage = 5;
 
@@ -116,8 +123,8 @@ function [v_path_left, v_path_right, turn_time] = path_rot(target_angle, CW, rig
     v_path_right = [v_path_right, linspace(right_voltage, right_voltage, time_full_speed)];
     
     % Decelerate
-    v_path_left = [v_path_left, linspace(left_voltage, left_voltage, deccel_time/2)];
-    v_path_right = [v_path_right, linspace(right_voltage, -right_voltage*0.46, deccel_time/2)];
+    v_path_left = [v_path_left, linspace(left_voltage, left_voltage_start, deccel_time)];
+    v_path_right = [v_path_right, linspace(right_voltage, right_voltage_start, deccel_time)];
    
     turn_time = accel_time + time_full_speed + deccel_time;
 end
@@ -138,8 +145,8 @@ function [v_path_left, v_path_right, turn_time] = path_linear(target_distance, F
         left_voltage = -drive_voltage + left_voltage_start;
     end
     % Accel
-    v_path_left = linspace(left_voltage, left_voltage, accel_time);
-    v_path_right = linspace(right_voltage, right_voltage, accel_time);
+    v_path_left = linspace(left_voltage_start, left_voltage, accel_time);
+    v_path_right = linspace(right_voltage_start, right_voltage, accel_time);
     
     % Hold
     dist_full_speed = abs(target_distance) - accel_dist - deccel_dist;
@@ -148,9 +155,16 @@ function [v_path_left, v_path_right, turn_time] = path_linear(target_distance, F
     v_path_right = [v_path_right, linspace(right_voltage, right_voltage, time_full_speed)];
     
     % Decelerate
-    v_path_left = [v_path_left, linspace(left_voltage, 0, deccel_time)];
-    v_path_right = [v_path_right, linspace(right_voltage, 0, deccel_time)];
+    v_path_left = [v_path_left, linspace(left_voltage, abs(left_voltage*0.46), deccel_time)];
+    v_path_right = [v_path_right, linspace(right_voltage, abs(right_voltage*0.46), deccel_time)];
     turn_time = accel_time + time_full_speed + deccel_time;
 
 
 end
+
+function [v_path_left, v_path_right, turn_time] = path_stop(volt_stop, time_stop)
+    v_path_left = linspace(volt_stop, volt_stop, time_stop);
+    v_path_right = linspace(volt_stop, volt_stop, time_stop);
+    turn_time = time_stop;
+end
+
