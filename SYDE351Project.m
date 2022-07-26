a923e177946eb4e29d0c375f7d2e3c2baaf69393
Lana Tomlin <lana.motom@gmail.com>
2022-07-26 11:20:22 -0400

    Suspension system tuning

--- SYDE351Project.m
+++ SYDE351Project.m
@@ -11,8 +11,8 @@ If = 1.15;
 Cshaft = 1;
 
 %% Suspension Parameters
-c = 100;
-k = 1000;
+c = 10;
+k = 10;
 speed = 1; %m/s
 
 wheel_separation = 0.5; %m
@@ -39,7 +39,7 @@ Ddelay = 0;
 floorsig = sim("floorsignal.slx", 5);
 doorsig = sim("DoorSignal.slx", 5.1);
 
-sim("Suspension_System.slx", 5.5);
+sim("Suspension_System.slx", 5.2);
 
 
 
